3317d5339f2cf56b14eab7d887f2181693026c9e
Jackson Ricardo Schroeder <jacksonrs@gmail.com>
2018-05-28 17:45:28 -0300

    stable version

--- exercicio03.js
+++ exercicio03.js
@@ -1,1 +1,6 @@
-console.log($)
+$(document).ready(() => {
+  $('#cpf').mask('000.000.000-00')
+  $('#rg').mask('9999999')
+  $('#dtNascimento').mask('00/00/0000')
+  $('input[type=tel]').mask('(99) 09999-9999')
+})
